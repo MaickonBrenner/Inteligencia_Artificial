8e24b132170bc72d5f2ede659aa8abc2a25ce085
Maickon_Brenner <maickonbrenner@gmail.com>
2025-03-15 09:27:44 -0300

    Publicando materiais

---
+++ Trabalho_AV1/regressao.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.loadtxt("atividade_enzimatica.csv", delimiter=',')
+
+# 1 - Temperatura, 2 - ph da Solução, 3 - Nivel de atividade enzimática
+
