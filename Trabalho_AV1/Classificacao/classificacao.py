cb9c8a155de742640d85f406bd9e3e7598447e5e
Maickon_Brenner <maickonbrenner@gmail.com>
2025-03-15 14:29:27 -0300

    Adicionando novas variaveis

--- Trabalho_AV1/Classificacao/classificacao.py
+++ Trabalho_AV1/Classificacao/classificacao.py
@@ -4,12 +4,18 @@ import matplotlib.pyplot as plt
 data = np.loadtxt("EMGsDataset.csv", delimiter=',')
 data = data.T
 
-c1, c2, c3 = 2,4,5
+c1, c2, c3, c4, c5 = 1, 2, 3, 4, 5
 
-plt.scatter(data[data[:,-1]==c1,0],data[data[:,-1]==c1,1], label='Sorrindo',c='yellow',ec='k')
-plt.scatter(data[data[:,-1]==c2,0],data[data[:,-1]==c2,1], label='Surpreso',c='teal',ec='k')
-plt.scatter(data[data[:,-1]==c3,0],data[data[:,-1]==c3,1], label='Rabugento',c='r',ec='k')
+plt.scatter(data[data[:,-1]==c1,0],data[data[:,-1]==c1,1], label='Neutro',c='yellow',ec='k')
+plt.scatter(data[data[:,-1]==c2,0],data[data[:,-1]==c2,1], label='Sorrindo',c='g',ec='k') # teal
+plt.scatter(data[data[:,-1]==c3,0],data[data[:,-1]==c3,1], label='Sobrancelhas Levantadas',c='m',ec='k')
+plt.scatter(data[data[:,-1]==c4,0],data[data[:,-1]==c4,1], label='Surpreso',c='b',ec='k')
+plt.scatter(data[data[:,-1]==c5,0],data[data[:,-1]==c5,1], label='Rabugento',c='r',ec='k')
 plt.xlabel("Corrugador do Supercílio")
 plt.ylabel("Zigomático Maior")
 plt.legend()
+
+
+
+
 plt.show()
